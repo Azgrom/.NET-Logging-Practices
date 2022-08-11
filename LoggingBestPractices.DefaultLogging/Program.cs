45fa2ac3e8c34a2742d507d2d6e1ad0b6eaa45ab
Rafael Lúcio <rafaellucio@pm.me>
2022-08-10 23:04:37 -0300

    Move package dependencies to a single `csproj` and create default logging project

---
+++ LoggingBestPractices.DefaultLogging/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Warning));
+
+var logger = new Logger<Program>(loggerFactory);
+
+for (int i = 0; i < 100_000_000; i++)
+{
+    // logger.LogInformation("Random number {RandomNumber}", Random.Shared.Next());
+    logger.LogInformation($"Random number {Random.Shared.Next()}");
+}
