810234ba4512d101ac65ef833032717dfcb0d5e2
Rafael Lúcio <rafaellucio@pm.me>
2024-10-14 00:18:37 -0300

    feat: adjust benchmark printing results

--- LoggingBestPractices.Benchmarks/CompareEmptyLogsBenchmark.cs
+++ LoggingBestPractices.Benchmarks/CompareEmptyLogsBenchmark.cs
@@ -2,18 +2,20 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Core;
 using Serilog.Events;
-using ILogger = Serilog.ILogger;
 
 namespace LoggingBestPractices.Benchmarks;
 
-[CategoriesColumn]
 [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
 [MemoryDiagnoser]
-[ThreadingDiagnoser]
 [RPlotExporter]
 public class CompareEmptyLogsBenchmark
 {
+    private const string ConstantLogMessageCategory = "Compile-Time Constant Log Template Category";
+    private const string FormattedLogMessageCategory = "Formatted Log Template Category";
+    private const string InterpolatedLogMessageCategory = "Interpolated Log Template Category";
+    private const string ParameterizedLogMessageCategory = "Parameterized Log Template Category";
     private const string LogMessageWithParameters =
         "This is a log message with parameters {0}, {1} and {2}";
 
@@ -24,7 +26,7 @@ public class CompareEmptyLogsBenchmark
     private readonly ILoggerFactory _loggerFactory
         = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Warning));
 
-    private readonly ILogger _serilogLogger;
+    private readonly Logger _serilogLogger;
 
     public CompareEmptyLogsBenchmark()
     {
@@ -33,24 +35,28 @@ public class CompareEmptyLogsBenchmark
     }
 
     [Benchmark]
+    [BenchmarkCategory(ConstantLogMessageCategory)]
     public void ConstantTemplate_DefaultLogger_Without_If()
     {
         _defaultLogger.LogInformation(ConstantLogMessageTemplate);
     }
 
     [Benchmark]
+    [BenchmarkCategory(ConstantLogMessageCategory)]
     public void ConstantTemplate_DefaultLogger_With_If()
     {
         if (_defaultLogger.IsEnabled(LogLevel.Information)) _defaultLogger.LogInformation(ConstantLogMessageTemplate);
     }
 
     [Benchmark]
+    [BenchmarkCategory(ParameterizedLogMessageCategory)]
     public void Parameterized_DefaultLogger_Without_If_WithParams()
     {
         _defaultLogger.LogInformation(LogMessageWithParameters, 50, 75, 125);
     }
 
     [Benchmark]
+    [BenchmarkCategory(ParameterizedLogMessageCategory)]
     public void Parameterized_DefaultLogger_With_If_WithParams()
     {
         if (_defaultLogger.IsEnabled(LogLevel.Information))
@@ -58,12 +64,14 @@ public class CompareEmptyLogsBenchmark
     }
 
     [Benchmark]
+    [BenchmarkCategory(FormattedLogMessageCategory)]
     public void Formatted_DefaultLogger_Without_If_WithParams()
     {
         _defaultLogger.LogInformation(string.Format(LogMessageWithParameters, 5, 7, 2));
     }
 
     [Benchmark]
+    [BenchmarkCategory(FormattedLogMessageCategory)]
     public void Formatted_DefaultLogger_With_If_WithParams()
     {
         if (_defaultLogger.IsEnabled(LogLevel.Information))
@@ -71,12 +79,14 @@ public class CompareEmptyLogsBenchmark
     }
 
     [Benchmark]
+    [BenchmarkCategory(InterpolatedLogMessageCategory)]
     public void Interpolated_DefaultLogger_Without_If_WithParams()
     {
         _defaultLogger.LogInformation($"This is a log message with parameters {464}, {4542}, {0}");
     }
 
     [Benchmark]
+    [BenchmarkCategory(InterpolatedLogMessageCategory)]
     public void Interpolated_DefaultLogger_With_If_WithParams()
     {
         if (_defaultLogger.IsEnabled(LogLevel.Information))
@@ -84,21 +94,25 @@ public class CompareEmptyLogsBenchmark
     }
 
     [Benchmark]
+    [BenchmarkCategory(ConstantLogMessageCategory)]
     public void ConstantTemplate_SerilogLogger_Without_If() { _serilogLogger.Information(ConstantLogMessageTemplate); }
 
     [Benchmark]
+    [BenchmarkCategory(ConstantLogMessageCategory)]
     public void ConstantTemplate_SerilogLogger_With_If()
     {
         if (_serilogLogger.IsEnabled(LogEventLevel.Information)) _serilogLogger.Information(ConstantLogMessageTemplate);
     }
 
     [Benchmark]
+    [BenchmarkCategory(ParameterizedLogMessageCategory)]
     public void Parameterized_SerilogLogger_Without_If_WithParams()
     {
         _serilogLogger.Information(LogMessageWithParameters, 50, 75, 125);
     }
 
     [Benchmark]
+    [BenchmarkCategory(ParameterizedLogMessageCategory)]
     public void Parameterized_SerilogLogger_With_If_WithParams()
     {
         if (_serilogLogger.IsEnabled(LogEventLevel.Information))
@@ -106,12 +120,14 @@ public class CompareEmptyLogsBenchmark
     }
 
     [Benchmark]
+    [BenchmarkCategory(FormattedLogMessageCategory)]
     public void Formatted_SerilogLogger_Without_If_WithParams()
     {
         _serilogLogger.Information(string.Format(LogMessageWithParameters, 5, 7, 2));
     }
 
     [Benchmark]
+    [BenchmarkCategory(FormattedLogMessageCategory)]
     public void Formatted_SerilogLogger_With_If_WithParams()
     {
         if (_serilogLogger.IsEnabled(LogEventLevel.Information))
@@ -119,12 +135,14 @@ public class CompareEmptyLogsBenchmark
     }
 
     [Benchmark]
+    [BenchmarkCategory(InterpolatedLogMessageCategory)]
     public void Interpolated_SerilogLogger_Without_If_WithParams()
     {
         _serilogLogger.Information($"This is a log message with parameters {464}, {4542}, {0}");
     }
 
     [Benchmark]
+    [BenchmarkCategory(InterpolatedLogMessageCategory)]
     public void Interpolated_SerilogLogger_With_If_WithParams()
     {
         if (_serilogLogger.IsEnabled(LogEventLevel.Information))
